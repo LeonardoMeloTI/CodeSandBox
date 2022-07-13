8f4f08d9cc4d96ded8be9a2f05b47f85a4e7948d
LeonardoMeloTI <53881848+LeonardoMeloTI@users.noreply.github.com>
2022-07-13 18:22:29 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Olá pela sua atenção</h1>
+      <h2>Aprendendo a mexer com react nessa plataforma!</h2>
+    </div>
+  );
+}
